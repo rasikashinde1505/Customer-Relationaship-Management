2920885b11ffcc54d6e80fcc49afb52be57e7a08
rasuu1996 <58535289+rasuu1996@users.noreply.github.com>
2020-04-19 16:48:00 +0200

    service implement

---
+++ Web-Customer-Tracker/src/com/gk/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.gk.service;
+
+import java.util.List;
+
+import com.gk.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomers(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
+}
