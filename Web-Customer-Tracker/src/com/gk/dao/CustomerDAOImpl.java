761f9bcf29b3935782468368564bfdf502ece16b
rasuu1996 <58535289+rasuu1996@users.noreply.github.com>
2020-04-19 16:41:41 +0200

    DAO implementation

---
+++ Web-Customer-Tracker/src/com/gk/dao/CustomerDAOImpl.java
@@ -0,0 +1,106 @@
+package com.gk.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import com.gk.entity.Customer;
+
+// Always applied to DAO implementation
+@Repository
+public class CustomerDAOImpl implements CustomerDao {
+
+	// need to inject session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	// Using Transaction so that we dont need to start and end the session
+	@Override
+	public List<Customer> getCustomers() {
+
+		// get the current Hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// create a query...sort by lastname
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
+
+		// execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+
+		// return the results
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// save/update the customer to the db
+		currentSession.saveOrUpdate(theCustomer);
+
+	}
+
+	@Override
+	public Customer getCustomers(int theId) {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// get the data from the db based on the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+
+		return theCustomer;
+	}
+
+	@Override
+	public void deleteCustomer(int theId) {
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// get the data from the db based on the primary key
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId ");
+		theQuery.setParameter("customerId", theId);
+		
+		theQuery.executeUpdate();
+	}
+
+	@Override
+	public List<Customer> searchCustomers(String theSearchName) {
+
+		  // get the current hibernate session
+		Session currentSession=sessionFactory.getCurrentSession();
+		
+		Query theQuery= null;
+		
+		   //
+        // only search by name if theSearchName is not empty
+        //
+        if (theSearchName != null && theSearchName.trim().length() > 0) {
+
+            // search for firstName or lastName ... case insensitive
+            theQuery =currentSession.createQuery("from Customer where lower(firstName) like :theName or lower(lastName) like :theName", Customer.class);
+            theQuery.setParameter("theName", "%" + theSearchName.toLowerCase() + "%");
+
+        }
+        else {
+            // theSearchName is empty ... so just get all customers
+            theQuery =currentSession.createQuery("from Customer", Customer.class);            
+        }
+        
+        // execute query and get result list
+        List<Customer> customers = theQuery.getResultList();
+                
+        // return the results        
+        return customers;
+        
+    
+		
+	}
+
+}
---
+++ Web-Customer-Tracker/src/com/gk/dao/CustomerDao.java
@@ -0,0 +1,17 @@
+package com.gk.dao;
+
+import java.util.List;
+
+import com.gk.entity.Customer;
+
+public interface CustomerDao {
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomers(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
+}
